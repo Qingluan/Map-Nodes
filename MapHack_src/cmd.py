3216e0120d0b32e97b7733f1ab41c3b053099b76
dr <dr@users-MacBook-Pro.local>
2019-06-05 19:15:23 +0800

    pok

--- MapHack_src/cmd.py
+++ MapHack_src/cmd.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import json
 from MapHack_src.remote import  run_server
@@ -5,35 +6,44 @@ from MapHack_src.remote import  Comunication
 from MapHack_src.task import Task
 
 parser = argparse.ArgumentParser(usage="Manager project, can create git , sync , encrypt your repo")
-parser.add_argument("-i","--init", help="default to initialize a projet in current dir")
 parser.add_argument("-c","--conf", help="use config json  file ,format like ss.")
-parser.add_argument("-s","--start", default=False, aciton='store_true', help="start server")
+parser.add_argument("-s","--start", default=False, action='store_true', help="start server")
 parser.add_argument("-a","--app", nargs="*", help="set app name")
 parser.add_argument("-t","--as", default='ip', help="set args ip/host")
 parser.add_argument("-o","--option", default='', help="set option default: ''")
 parser.add_argument("-S","--session", default='default', help="set option default: default")
-
-
+parser.add_argument("-T","--test", default=False, action='store_true', help="test client ")
 
 
 def main():
     args = parser.parse_args()
-    
+    w = None
+    f = args.conf
+    with open(f) as fp:
+        w = json.loads(fp)
+        assert  'server' in w
+        assert  'server_port' in w
+        assert  'password' in w
+        assert  'method' in w
     if args.start:
-        f = args.conf
-        with open(f) as fp:
-            w = json.loads(fp)
-            assert  'server' in w
-            assert  'server_port' in w
-            assert  'password' in w
-            assert  'method' in w
-            run_server(w)
-
+        run_server(w)
+    assert  w is not None
     if args.app:
         app = args.app[0]
         target = args.app[1]
         data = Task.build_json(app, session=args.session, **{getattr(args,'as'): target})
-        
+        res = Comunication.SendOnce(w, data)
+        print(res)
+        sys.exit(0)
+
+    if args.test:
+        data = Task.build_json("", session=args.session, op="test")
+        res = Comunication.SendOnce(w, data)
+        print(res)
+        sys.exit(0)
+
+
+
 
 if __name__ == "__main__":
     main()
