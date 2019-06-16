9364382db09a486649792d3328270105f7c8afab
Qingluan <you@example.com>
2019-06-16 21:30:53 +0800

    fix process bug

--- MapHack_src/task.py
+++ MapHack_src/task.py
@@ -585,8 +585,8 @@ class Task:
         elif op == 'upgrade':
             data = Task.build_json('', op="upgrade-local", session=self._session)
             w = self._pconf
-            if os.path.exists(os.path.expanduser(self.conf['base']['this'])):
-                os.remove(os.path.expanduser(self.conf['base']['this']))
+            if os.path.exists(os.path.expanduser('~/.maper.ini')):
+                os.remove(os.path.expanduser('~/.maper.ini'))
             if 'mark' not in self._pconf:
                 w['server'] = 'localhost'
                 w['server_port'] = str(int(w['server_port']) + 1)
