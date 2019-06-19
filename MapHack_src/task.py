6bdddab6990f3a40d640bc36e7025458a4f05031
root <root@localhost.localdomain>
2019-06-19 03:49:30 -0400

    ok

--- MapHack_src/task.py
+++ MapHack_src/task.py
@@ -512,7 +512,9 @@ class Task:
             return 1, 'no task_root'
 
     async def update_config(self):
-        code, res = await run_shell('wget -c -t 10 "https://raw.githubusercontent.com/Qingluan/Map-Nodes/master/template.ini" -O $HOME/.maper.ini', background=False)
+        code, res = await run_shell('wget -c -t 10 "https://raw.githubusercontent.com/Qingluan/Map-Nodes/master/template.ini" -O- >  $HOME/.maper2.ini', background=False)
+        if os.path.exists(os.path.expanduser("~/.maper2.ini")):
+            os.rename(os.path.expanduser("~/.maper2.ini"), os.path.expanduser("~/.maper.ini"))
         self.conf = get_local_config()
         if not res:
             res = 'try to update maper.ini'
